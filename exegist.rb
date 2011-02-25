3b8ea0dc09e57a9c243d68096b80296e7b64cbce
David Phillips <me@davidphillips.us>
2011-02-25 01:25:17 -0500

    began ruby setup even started the rb page

--- exegist.rb
+++ exegist.rb
@@ -1,3 +1,27 @@
+require 'rubygems'
+require 'sinatra'
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-timestamps'
+
+get '/' do
+  @title = "Welcome to EXEgist"
+  erb :welcome :layout => false
+end
+
+get '/joyce' do
+  @title="Joyce"
+  erb :standard 
+end
+
+get '/paper2' do
+  erb :standard
+end
+
+get '/paper3' do
+  erb :standard
+end
+
 
 #GREG'S WAY OF PARSING THE SENTENCES SERVER SIDE
 #<% for sentence in @mything.split(".") %><span class="aSentence"><%= sentence %></span><% end %>
