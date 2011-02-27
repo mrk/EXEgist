ffeeb5d447290c9c86d6f56c7cc04def3b77d6f5
mrk <mark.triant@gmail.com>
2011-02-27 17:01:28 -0500

    l'init git commit

--- exegist.rb
+++ exegist.rb
@@ -13,11 +13,27 @@ get '/' do
   erb :welcome :layout => false
 end
 
-get '/paper/:id' do
-  @title="Joyce"
-  erb :standard 
+get '/wallace' do
+  @title="E Unibus Plurum"
+  erb :wallace 
+end
+
+get '/paper' do
+  @title="(sava class paper)"
+  erb :paper 
+end
+
+get '/fanfic' do
+  @title="(fanfic name)"
+  erb :fanfic 
 end
 
 
+
+
 #GREG'S WAY OF PARSING THE SENTENCES SERVER SIDE
+# also add:   - generate sequential unique ids
+#             - put each one in a db
+#             -- each entry associated with 
+#             - each span given a set of classes, eg. <span class
 #<% for sentence in @mything.split(".") %><span class="aSentence"><%= sentence %></span><% end %>
