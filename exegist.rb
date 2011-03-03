a6eee7567736e19f285005a0738897002b1a901a
mrk <mark.triant@gmail.com>
2011-03-03 16:08:38 -0500

    didn't change much

--- exegist.rb
+++ exegist.rb
@@ -4,6 +4,7 @@ require 'dm-core'
 require 'dm-migrations'
 require 'dm-timestamps'
 require 'dm-validations'
+require 'dm-types'
 
 
 #NOTE ON COMMENTS
@@ -12,6 +13,36 @@ require 'dm-validations'
 configure :development do
   DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/exegist.db")
 end
+
+helpers do
+  
+  # Check to see if a user is logged in
+  def logged_in?
+    if request.cookies['userid']
+      true
+    else
+      false
+    end
+  end
+
+  # Add this to the top of a route to make it accessible only to logged in users
+  def authorize!
+    redirect '/' unless logged_in?
+  end
+
+  # Get the logged in user's ID
+  def get_userid
+    request.cookies['userid']
+  end
+
+  # Set the logged in user's ID (logging them in)
+  def set_userid(id)
+    response.set_cookie('userid', id)
+  end
+
+end
+
+
 
 # mark's first pass at defining our model
 class User
@@ -33,10 +64,13 @@ class User
   property :password,   String
   property :created_at, DateTime
   
+  attr_accessor :password_confirmation
+  validates_confirmation_of :password
+  
   has n, :comments
 end
 
-class Comment                            # classes map to tables
+class Comment                         # classes map to tables
   include DataMapper::Resource
   
   property :id,           Serial      # properties map to fields
@@ -53,14 +87,62 @@ end
 configure :development do
   DataMapper .auto_upgrade!
 end
-  
 
-
+mypage = ''
 
 get '/' do
   @title = "Welcome to EXEgist"
   erb :welcome
 end
+
+# Show a login form or log the user out
+get '/login/?' do
+  unless logged_in?
+    erb :login
+  else
+    flash[:notice] = 'You have been logged out.'
+    response.delete_cookie('userid')
+    redirect '/'
+  end
+end
+
+post '/login/?' do
+  user = User.first(:username => params[:username])
+  if user
+    if user.password == params[:password]
+      set_userid(user.id)
+      flash[:notice] = 'You are now logged in.'
+      redirect '/'
+    else
+      flash[:notice] = 'Incorrect password.'
+      redirect '/login'
+    end
+  else
+    flash[:notice] = 'Incorrect username.'
+    redirect '/login'
+  end
+end
+
+
+
+get '/wallace' do
+  @title="E Unibus Plurum"
+  mypage = "wallace"  
+  erb :wallace 
+end
+
+get '/paper' do
+  @title="(sava class paper)"
+  mypage = "paper"
+  erb :paper 
+end
+
+get '/fanfic' do
+  @title="(fanfic name)"
+  mypage = "fanfic"
+  erb :fanfic
+end
+
 
 get '/register' do
   @title = "EXEgist User Registration"
@@ -69,27 +151,21 @@ end
 
 post '/newuser' do
   @user         = User.new(params[:user])
+  
   if @user.save
-    redirect("/")
+    redirect('/'+mypage)
   else
-    redirect('/')
+    redirect('/'+mypage)
+    my_account.errors.each do |e|
+      puts e
+    end
   end
 end
 
-get '/wallace' do
-  @title="E Unibus Plurum"
-  erb :wallace 
+get '/*' do
+  erb :notfound
 end
 
-get '/paper' do
-  @title="(sava class paper)"
-  erb :paper 
-end
-
-get '/fanfic' do
-  @title="(fanfic name)"
-  erb :fanfic 
-end
 
 
 
